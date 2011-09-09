7f38e90594a4410ec586fceb2e43201591fe5120
pyromaniac <kinwizard@gmail.com>
2011-09-10 00:19:30 +0400

    Fixed select component contexts

--- app/components/select_component.rb
+++ app/components/select_component.rb
@@ -3,9 +3,9 @@ class SelectComponent < BaseComponent
   def form
     @options = case field.options[:select]
       when Symbol then
-        parent_controller.send field.options[:select]
+        parent_controller.view_context.send field.options[:select]
       when Proc then
-        field.options[:select].bind(self).call
+        field.options[:select].bind(parent_controller.view_context).call
       else
         field.options[:select]
     end
