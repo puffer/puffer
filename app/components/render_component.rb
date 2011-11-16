adafe813506b4d342d3fcfb66ff1e938eed9b98c
pyromaniac <kinwizard@gmail.com>
2011-11-16 11:30:54 +0400

    Added symbol option to render component

--- app/components/render_component.rb
+++ app/components/render_component.rb
@@ -1,7 +1,13 @@
 class RenderComponent < Puffer::Component::Base
 
   def action_missing name
-    field.options[:render].bind(self).call
+    case field.options[:render]
+    when Symbol then
+      send(field.options[:render])
+    when Proc then
+      field.options[:render].bind(self).call
+    else ''
+    end
   end
 
 end
